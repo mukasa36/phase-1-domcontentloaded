d02112458acd0745c0d0baae7b50c43579c1db87
emmanuel <emmanuel.mukasa@moringaschool.com>
2023-06-29 00:08:12 +0300

    Assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+// DOMContentLoaded
+
+document.addEventListener("DOMContentLoaded", function() {
+    let txt = document.getElementById("text");
+     let modify =txt.innerHTML="This is really cool!"
+
+    // console.log( txt.innerText("hello world"));
+    return modify;
+  });
+
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
